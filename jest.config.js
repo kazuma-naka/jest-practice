f418e88ef192663b7fc9419b07f9c35f41ecdd83
kazuma-naka <kazuma.naka.01@gmail.com>
2024-12-16 10:45:07 -0500

    first commit

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+
+const config = {
+    roots: [
+      "<rootDir>/src"
+    ],
+    testMatch: [
+      "**/?(*.)+(spec|test).[jt]s?(x)"
+    ],
+    transform: {
+      "\\.(ts|tsx)$": "ts-jest"
+    },
+  };
+  
+  module.exports = config;
